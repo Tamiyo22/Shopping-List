a573551c991c2f2d97a688f96cd6882ad9317e2b
Tamiyo22 <melissaeaybar@gmail.com>
2019-05-18 15:01:30 -0500

    added disptach and payloads Finished setting up redux. Connected back and front ends.

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,11 +1,47 @@
-//request to the backend should go here eventually
-
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from './types';
-
+import axios from 'axios'
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from './types';
 
 
-export const getItems =()=> {
+
+export const getItems =()=> dispatch => {
+	dispatch(setItemsLoading());
+	axios
+	.get('/api/items')
+	.then(response =>
+		dispatch({
+			type: GET_ITEMS,
+			payload: response.data
+		})
+		)
+};
+
+export const addItem =(item)=> dispatch => {
+	axios
+	.post('/api/items', item)
+	.then(response =>
+		dispatch({
+			type: ADD_ITEM,
+			payload: response.data
+		}))
+
+};
+
+
+
+export const deleteItem =(id)=> dispatch => {
+	axios.delete(`/api/items/${id}`).then(response =>
+		dispatch({
+			type:DELETE_ITEM,
+			payload:id
+		})
+		)
+};
+
+
+export const setItemsLoading  =()=> {
 	return {
-		type: GET_ITEMS,
+		type: ITEMS_LOADING
 	};
 };
+
+
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {Container,ListGroupItem,Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import uuid from 'uuid'
+// import uuid from 'uuid' removing this. using it only for the state
 import {connect} from 'react-redux';
-import {getItems} from '../actions/itemActions';
-import PropTypes from 'prop-types'
+import {getItems, deleteItem} from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component{
 
@@ -12,37 +12,27 @@ componentDidMount(){
 	this.props.getItems();
 }
 
+onDeleteClick = (id)=>{
+this.props.deleteItem(id)
+}
+
 render(){
 
 	const {items}= this.props.item
 	return (
  <Container>
- <Button
-	color="dark"
-	 style={{marginBotton:'2rem'}}
-   onClick={() => {const name = prompt('Enter Item');
-   if(name){
-	  this.setState(state => ({
-		 items:[...state.items, { id:uuid(),name }]
-	 }));
- }
-}}
->Add Item</Button>
+
 
  {/* <ListGroup> */}
   <TransitionGroup className="shopping-list">
-    {items.map(({id,name}) => (
-	   <CSSTransition key ={id} timeout={500} classNames= "fade">
+    {items.map(({_id,name}) => (
+	   <CSSTransition key ={_id} timeout={500} classNames= "fade">
 	<ListGroupItem>
 		<Button className="remove-btn"
 		color="danger"
 		size="sm"
-		onClick={()=>{
-			this.setState(state =>({
-				items : state.items.filter(item => item.id !== id)
-			}))
-		}
-		}>
+		onClick={this.onDeleteClick.bind(this,_id)}
+			>
 			&times;
 		</Button>
 		{name} </ListGroupItem>
@@ -65,4 +55,4 @@ const mapStateToProps=(state) => ({
 })
 
 
-export default connect(mapStateToProps,{getItems})(ShoppingList);
+export default connect(mapStateToProps,{getItems, deleteItem})(ShoppingList);
